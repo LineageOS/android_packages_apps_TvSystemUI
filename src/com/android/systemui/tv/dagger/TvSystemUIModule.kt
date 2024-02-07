c07159a9afbd2445318e0a67afd1b8ef2a1d821c
Luca Zuccarini <acul@google.com>
2024-02-07 11:42:11 +0000

    Rename remaining animations classes to cover transitions in general.
    Soon they will be used for both launches and returns, so these names
    are more accurate.
    
    Bug: 323863002
    Flag: NA
    Test: still builds (no functionality change)
    Change-Id: I261c8503c4e11f36254385486062e3c7bb4db2dc

--- src/com/android/systemui/tv/dagger/TvSystemUIModule.kt
+++ src/com/android/systemui/tv/dagger/TvSystemUIModule.kt
@@ -26,7 +26,7 @@ import com.android.internal.logging.UiEventLogger
 import com.android.keyguard.KeyguardViewController
 import com.android.settingslib.bluetooth.LocalBluetoothManager
 import com.android.systemui.Dependency
-import com.android.systemui.animation.DialogLaunchAnimator
+import com.android.systemui.animation.DialogTransitionAnimator
 import com.android.systemui.broadcast.BroadcastSender
 import com.android.systemui.dagger.ReferenceSystemUIModule
 import com.android.systemui.dagger.SysUISingleton
@@ -200,7 +200,7 @@ abstract class TvSystemUIModule {
             broadcastSender: BroadcastSender,
             notifCollection: CommonNotifCollection,
             uiEventLogger: UiEventLogger,
-            dialogLaunchAnimator: DialogLaunchAnimator,
+            dialogTransitionAnimator: DialogTransitionAnimator,
             nearbyMediaDevicesManager: NearbyMediaDevicesManager,
             audioManager: AudioManager,
             powerExemptionManager: PowerExemptionManager,
@@ -210,7 +210,7 @@ abstract class TvSystemUIModule {
             ): MediaOutputDialogFactory =
                 TvMediaOutputDialogFactory(context, mediaSessionManager, localBluetoothManager,
                         activityStarter, broadcastSender, notifCollection, uiEventLogger,
-                        dialogLaunchAnimator, nearbyMediaDevicesManager, audioManager,
+                        dialogTransitionAnimator, nearbyMediaDevicesManager, audioManager,
                         powerExemptionManager, keyguardManager, featureFlags, userTracker)
     }
 }
