b4b53f3a02d70ae82cfa25f560487ed2b7afb82a
Fabian Kozynski <kozynski@google.com>
2025-09-29 12:11:46 -0700

    Remove BrightnessSliderModule
    Test: build
    Flag: EXEMPT FLAG_REMOVAL
    Bug: 444180543
    Change-Id: Id695d2cb7244c156b5764f0625ed45a96936ec6e

--- src/com/android/systemui/tv/dagger/TvSystemUIModule.kt
+++ src/com/android/systemui/tv/dagger/TvSystemUIModule.kt
@@ -52,7 +52,6 @@ import com.android.systemui.qs.tileimpl.QSFactoryImpl
 import com.android.systemui.screenshot.ReferenceScreenshotModule
 import com.android.systemui.settings.MultiUserUtilsModule
 import com.android.systemui.settings.UserTracker
-import com.android.systemui.settings.brightness.dagger.BrightnessSliderModule
 import com.android.systemui.shade.ShadeEmptyImplModule
 import com.android.systemui.statusbar.NotificationListener
 import com.android.systemui.statusbar.NotificationLockscreenUserManager
@@ -107,7 +106,6 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
             AccessibilityModule::class,
             AccessibilityRepositoryModule::class,
             AospPolicyModule::class,
-            BrightnessSliderModule::class,
             ConnectingDisplayViewModel.StartableModule::class,
             GestureModule::class,
             HdmiModule::class,
